0ef6809d8753b745eeecb9b6ca896493a249875f
suttonjoseph60 <suttonjoseph60@gmail.com>
2026-05-18 15:59:11 -0700

    uploading problem 2

--- midterm_master/Midterm_Core5/Palindrome/Program.cs
+++ midterm_master/Midterm_Core5/Palindrome/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using System;
+using System.Collections.Generic;
 
 namespace Palindrome
 {
@@ -6,7 +8,61 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* Main pseudocode:
+             * Console.WriteLine("Enter a word or phrase to check if it is a palindrome.");
+             * get string input = Console.ReadLine();
+             * print boolean result of IsPalindrome(input) as a string statement.
+             * WriteLine($"Is {input} a palindrome? " + IsPalindrome(input));
+             */
+
+            Console.WriteLine("Enter a word or phrase to check if it is a palindrome.");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Is {input} a palindrome? " + IsPalindrome(input));
+        }
+
+        /*
+         IsPalindrome pseudocode:
+         static bool IsPalindrome(string input)
+            {
+                create stack and queue data structures
+                for each character in input that isnt a punctuation, whitespace
+                    add character to stack and queue
+                for each character in input
+                    if character from top of stack != character from front of queue
+                        return false
+                return true
+            }
+         */
+        static bool IsPalindrome(string input)
+        {
+            if (input != null)
+            {
+                Stack<char> stack = new Stack<char>();
+                Queue<char> queue = new Queue<char>();
+                foreach (char c in input)
+                {
+                    if (char.IsLetterOrDigit(c))
+                    {
+                        char lowerChar = char.ToLower(c);
+                        stack.Push(lowerChar);
+                        queue.Enqueue(lowerChar);
+                    }
+                }
+                while (stack.Count > 0 && queue.Count > 0)
+                {
+                    if (stack.Pop() != queue.Dequeue())
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+
         }
     }
 }
