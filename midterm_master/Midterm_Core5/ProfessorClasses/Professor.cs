c2a2d9eeb8b4c79f38b2890055b637caee561a58
suttonjoseph60 <suttonjoseph60@gmail.com>
2026-05-18 16:40:33 -0700

    final upload for problem 3

--- midterm_master/Midterm_Core5/ProfessorClasses/Professor.cs
+++ midterm_master/Midterm_Core5/ProfessorClasses/Professor.cs
@@ -1,8 +1,60 @@
-﻿using System;
+﻿
+using System;
 
 namespace ProfessorClasses
 {
     public class Professor
     {
+        // Attributes
+        private string lNumber;
+        private string firstName;
+        private string lastName;
+        private string department;
+        
+        // Constructor
+        public Professor(string lNumber, string firstName, string lastName, string department)
+        {
+            this.lNumber = lNumber;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.department = department;
+        }
+
+        // default constructor
+        public Professor()
+            {
+            lNumber = "Unknown";
+            firstName = "Unknown";
+            lastName = "Unknown";
+            department = "Unknown";
+        }
+
+        // Getters and Setters
+        public string LNumber
+        {
+            get { return lNumber; }
+            set { lNumber = value; }
+        }
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public string Department
+        {
+            get { return department; }
+            set { department = value; }
+        }
+        
+        // ToString method
+        public override string ToString()
+        {
+            return $"Professor: {firstName} {lastName}, LNumber: {lNumber}, Department: {department}";
+        }
     }
 }
--- midterm_master/Midterm_Core5/ProfessorTests/Program.cs
+++ midterm_master/Midterm_Core5/ProfessorTests/Program.cs
@@ -7,7 +7,42 @@ namespace ProfessorTests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TestProfessorConstructor();
+            TestProfessorToString();
+            TestProfessorGetters();
+            TestProfessorSetters();
+        }
+
+        static void TestProfessorConstructor()
+        {
+            Professor professor = new Professor("L12345", "Matt", "Flemmingson", "Building Stuff");
+            Console.WriteLine("Testing Professor Constructor:");
+            Console.WriteLine($"LNumber: {professor.LNumber}, FirstName: {professor.FirstName}, LastName: {professor.LastName}, Department: {professor.Department}");
+        }
+
+        static void TestProfessorToString()
+        {
+            Professor professor = new Professor("L00327638", "Dingle", "Doe", "Computer Science");
+            Console.WriteLine("Testing Professor ToString:");
+            Console.WriteLine(professor.ToString());
+        }
+
+        static void TestProfessorGetters()
+        {
+            Professor professor = new Professor("L00892376", "Mary", "Poppins", "Academy of Umbrella Flying");
+            Console.WriteLine("Testing Professor Getters:");
+            Console.WriteLine($"LNumber: {professor.LNumber}, FirstName: {professor.FirstName}, LastName: {professor.LastName}, Department: {professor.Department}");
+        }
+
+        static void TestProfessorSetters()
+        {
+            Professor professor = new Professor();
+            professor.LNumber = "L00543219";
+            professor.FirstName = "Jacob";
+            professor.LastName = "Martin";
+            professor.Department = "Mathematics";
+            Console.WriteLine("Testing Professor Setters:");
+            Console.WriteLine($"LNumber: {professor.LNumber}, FirstName: {professor.FirstName}, LastName: {professor.LastName}, Department: {professor.Department}");
         }
     }
 }
